c1c919030e5a9b230d010657b8cf5d212df701a4
Loki251106 <nguyenhoangnhatminh251106@gmail.com>
2026-02-24 02:47:22 +0700

    nop bai tap bien kieudl toan tu

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Happy developing ✨')
+/*/bai 1
+let ly,hoa,sinh;
+ly=prompt("moi ban nhap vao diem mon vat ly");
+hoa=prompt("moi ban nhap vao diem mon hoa");
+sinh=prompt("moi ban nhap vao diem mon sinh");
+let dtb=(Number(ly)+Number(hoa)+Number(sinh))/3;
+alert("diem trung binh cua ban la: "+dtb);
+// bai 2
+let C= prompt("moi ban nhap vao do C de chuyen doi sang do F");
+let F=C*1.8 +32;
+alert(C+ " do C sau khi chuyen sang do F la: "+F);
+*/
+/*
+bai 3 viet chuong trinh tinh dien tich hinh tron
+
+let r=prompt("moi ban nhap vao ban kinh hinh tron");
+let S=3.14*r*r;
+alert("dien tich hinh tron la: "+S);
+
+bai 4*/
+
+    let r=prompt("moi ban nhap vao ban kinh hinh tron");
+    let C=2*3.14*r;
+    alert("chu vi hinh tron ban vua nhap la: "+C);
